8e1610e69737bfcdafda25cf36c82b65a1912d1f
William Peru <wperu@z4r3p1.42lyon.fr>
2021-12-10 19:51:11 +0100

    add main stack and map

---
+++ main_map.cpp
@@ -0,0 +1,343 @@
+#include <iostream>
+#include "includes/map_fct.hpp"
+
+bool fncomp (char lhs, char rhs) {return lhs<rhs;}
+
+struct classcomp {
+  bool operator() (const char& lhs, const char& rhs) const
+  {return lhs<rhs;}
+};
+
+int main ()
+{
+ {
+  ft::map<char,int> first;
+  first['a']=10;
+  first['b']=30;
+  first['c']=50;
+  first['d']=70;
+  for (ft::map<char,int>::iterator it=first.begin(); it!=first.end(); ++it)
+    std::cout << it->first << " => " << it->second << '\n';
+  ft::map<char,int> second (first.begin(),first.end());
+
+  ft::map<char,int> third (second);
+
+  ft::map<char,int,classcomp> fourth;                 // class as Compare
+
+  bool(*fn_pt)(char,char) = fncomp;
+  ft::map<char,int,bool(*)(char,char)> fifth (fn_pt); // function pointer as Compare
+ }
+
+  
+
+  {
+    ft::map<char,int> mymap;
+
+  mymap['x'] = 100;
+  mymap['y'] = 200;
+  mymap['z'] = 300;
+
+  // show content:
+  ft::map<char,int>::reverse_iterator rit;
+  for (rit=mymap.rbegin(); rit!=mymap.rend(); ++rit)
+    std::cout << rit->first << " => " << rit->second << '\n';
+
+  } 
+  std::cout<<"-------------TEST SIZE------------"<<std::endl;
+  {
+
+    // accessing mapped values
+  ft::map<char,std::string> mymap;
+
+  mymap['a']="an element";
+  mymap['b']="another element";
+  mymap['c']=mymap['b'];
+
+  std::cout << "mymap['a'] is " << mymap['a'] << '\n';
+  std::cout << "mymap['b'] is " << mymap['b'] << '\n';
+  std::cout << "mymap['c'] is " << mymap['c'] << '\n';
+  std::cout << "mymap['d'] is " << mymap['d'] << '\n';
+
+  std::cout << "mymap now contains " << mymap.size() << " elements.\n";
+
+  }
+  std::cout<<"-------------TEST SIZE_MAX------------"<<std::endl;
+  {
+  int i;
+  ft::map<int,int> mymap;
+
+  if (mymap.max_size()>1000)
+  {
+    for (i=0; i<1000; i++) mymap[i]=0;
+    std::cout << "The map contains 1000 elements.\n";
+  }
+  else std::cout << "The map could not hold 1000 elements.\n";
+
+  }
+  
+std::cout<<"-------------TEST EMPTY------------"<<std::endl;
+
+{
+  // map::empty
+  ft::map<char,int> mymap;
+
+  mymap['a']=10;
+  mymap['b']=20;
+  mymap['c']=30;
+
+  while (!mymap.empty())
+  {
+    std::cout << mymap.begin()->first << " => " << mymap.begin()->second << '\n';
+    mymap.erase(mymap.begin());
+  }
+
+
+}
+std::cout<<"-------------TEST INSERT------------"<<std::endl;
+{
+  ft::map<char,int> mymap;
+
+  // first insert function version (single parameter):
+  mymap.insert ( ft::pair<char,int>('a',100) );
+  mymap.insert ( ft::pair<char,int>('z',200) );
+
+  ft::map<char,int>::iterator itt = mymap.begin();
+  mymap.insert ( ft::pair<char,int>('z',500) );
+  for (; itt!=mymap.end(); ++itt)
+    std::cout << itt->first << " => " << itt->second << '\n';
+
+
+  // second insert function version (with hint position):
+  ft::map<char,int>::iterator it = mymap.begin();
+  mymap.insert (it, ft::pair<char,int>('b',300));  // max efficiency inserting
+  mymap.insert (it, ft::pair<char,int>('c',400));  // no max efficiency inserting
+
+  // third insert function version (range insertion):
+  ft::map<char,int> anothermap;
+  anothermap.insert(mymap.begin(),mymap.find('c'));
+
+  // showing contents:
+  std::cout << "mymap contains:\n";
+  for (it=mymap.begin(); it!=mymap.end(); ++it)
+    std::cout << it->first << " => " << it->second << '\n';
+
+  std::cout << "anothermap contains:\n";
+  for (it=anothermap.begin(); it!=anothermap.end(); ++it)
+    std::cout << it->first << " => " << it->second << '\n';
+
+
+}
+std::cout<<"-------------TEST ERASE------------"<<std::endl;
+{
+  ft::map<char,int> mymap;
+  ft::map<char,int>::iterator it;
+
+  // insert some values:
+  mymap['a']=10;
+  mymap['b']=20;
+  mymap['c']=30;
+  mymap['d']=40;
+  mymap['e']=50;
+  mymap['f']=60;
+
+  it=mymap.find('b');
+  mymap.erase (it);                   // erasing by iterator
+
+  mymap.erase ('c');                  // erasing by key
+
+  it=mymap.find ('e');
+  mymap.erase ( it, mymap.end() );    // erasing by range
+
+  // show content:
+  for (it=mymap.begin(); it!=mymap.end(); ++it)
+    std::cout << it->first << " => " << it->second << '\n';
+
+
+}
+std::cout<<"-------------TEST SWAP------------"<<std::endl;
+
+{
+  ft::map<char,int> foo,bar;
+
+  foo['x']=100;
+  foo['y']=200;
+
+  bar['a']=11;
+  bar['b']=22;
+  bar['c']=33;
+
+  foo.swap(bar);
+
+  std::cout << "foo contains:\n";
+  for (ft::map<char,int>::iterator it=foo.begin(); it!=foo.end(); ++it)
+    std::cout << it->first << " => " << it->second << '\n';
+
+  std::cout << "bar contains:\n";
+  for (ft::map<char,int>::iterator it=bar.begin(); it!=bar.end(); ++it)
+    std::cout << it->first << " => " << it->second << '\n';
+
+ 
+}
+std::cout<<"-------------TEST CLEAR------------"<<std::endl;
+{
+  ft::map<char,int> mymap;
+
+  mymap['x']=100;
+  mymap['y']=200;
+  mymap['z']=300;
+
+  std::cout << "mymap contains:\n";
+  for (ft::map<char,int>::iterator it=mymap.begin(); it!=mymap.end(); ++it)
+    std::cout << it->first << " => " << it->second << '\n';
+
+  mymap.clear();
+  mymap['a']=1101;
+  mymap['b']=2202;
+
+  std::cout << "mymap contains:\n";
+  for (ft::map<char,int>::iterator it=mymap.begin(); it!=mymap.end(); ++it)
+    std::cout << it->first << " => " << it->second << '\n';
+
+
+}
+std::cout<<"-------------TEST KEY_COMP------------"<<std::endl;
+{
+  ft::map<char,int> mymap;
+
+  ft::map<char,int>::key_compare mycomp = mymap.key_comp();
+
+  mymap['a']=100;
+  mymap['b']=200;
+  mymap['c']=300;
+
+  std::cout << "mymap contains:\n";
+
+  char highest = mymap.rbegin()->first;     // key value of last element
+
+  ft::map<char,int>::iterator it = mymap.begin();
+  do {
+    std::cout << it->first << " => " << it->second << '\n';
+  } while ( mycomp((*it++).first, highest) );
+
+}
+std::cout<<"-------------TEST VALUE_COMP------------"<<std::endl;
+{
+  ft::map<char,int> mymap;
+
+  mymap['x']=1001;
+  mymap['y']=2002;
+  mymap['z']=3003;
+
+  std::cout << "mymap contains:\n";
+
+  ft::pair<char,int> highest = *mymap.rbegin();          // last element
+
+  ft::map<char,int>::iterator it = mymap.begin();
+  do {
+    std::cout << it->first << " => " << it->second << '\n';
+  } while ( mymap.value_comp()(*it++, highest) );
+
+
+}
+std::cout<<"-------------TEST FIND------------"<<std::endl;
+{
+  ft::map<char,int> mymap;
+  ft::map<char,int>::iterator it;
+
+  mymap['a']=50;
+  mymap['b']=100;
+  mymap['c']=150;
+  mymap['d']=200;
+
+  it = mymap.find('b');
+  if (it != mymap.end())
+    mymap.erase (it);
+
+  // print content:
+  std::cout << "elements in mymap:" << '\n';
+  std::cout << "a => " << mymap.find('a')->second << '\n';
+  std::cout << "c => " << mymap.find('c')->second << '\n';
+  std::cout << "d => " << mymap.find('d')->second << '\n';
+
+
+}
+std::cout<<"-------------TEST COUNT------------"<<std::endl;
+{
+  ft::map<char,int> mymap;
+  char c;
+
+  mymap ['a']=101;
+  mymap ['c']=202;
+  mymap ['f']=303;
+
+  for (c='a'; c<'h'; c++)
+  {
+    std::cout << c;
+    if (mymap.count(c)>0)
+      std::cout << " is an element of mymap.\n";
+    else 
+      std::cout << " is not an element of mymap.\n";
+  }
+
+
+}
+std::cout<<"-------------TEST UPPER_BOUND/LOWER_BOUND------------"<<std::endl;
+{
+  ft::map<char,int> mymap;
+  ft::map<char,int>::iterator itlow,itup;
+
+  mymap['a']=20;
+  mymap['b']=40;
+  mymap['c']=60;
+  mymap['d']=80;
+  mymap['e']=100;
+
+  itlow=mymap.lower_bound ('b');  // itlow points to b
+  itup=mymap.upper_bound ('d');   // itup points to e (not d!)
+
+  mymap.erase(itlow,itup);        // erases [itlow,itup)
+
+  // print content:
+  for (ft::map<char,int>::iterator it=mymap.begin(); it!=mymap.end(); ++it)
+    std::cout << it->first << " => " << it->second << '\n';
+
+}
+std::cout<<"-------------TEST EQUAL_RANGE------------"<<std::endl;
+{
+  ft::map<char,int> mymap;
+
+  mymap['a']=10;
+  mymap['b']=20;
+  mymap['c']=30;
+
+  ft::pair<ft::map<char,int>::iterator,ft::map<char,int>::iterator> ret;
+  ret = mymap.equal_range('b');
+
+  std::cout << "lower bound points to: ";
+  std::cout << ret.first->first << " => " << ret.first->second << '\n';
+
+  std::cout << "upper bound points to: ";
+  std::cout << ret.second->first << " => " << ret.second->second << '\n';
+
+
+}
+std::cout<<"-------------TEST GET_ALLOCATOR------------"<<std::endl;
+{
+  int psize;
+  ft::map<char,int> mymap;
+  ft::pair<const char,int>* p;
+
+  // allocate an array of 5 elements using mymap's allocator:
+  p=mymap.get_allocator().allocate(5);
+
+  // assign some values to array
+  psize = sizeof(ft::map<char,int>::value_type)*5;
+
+  std::cout << "The allocated array has a size of " << psize << " bytes.\n";
+
+  mymap.get_allocator().deallocate(p,5);
+
+
+}
+  return 0;
+}
---
+++ main_stack.cpp
@@ -0,0 +1,111 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_stack.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wperu <wperu@student.42lyon.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/13 18:06:08 by wperu             #+#    #+#             */
+/*   Updated: 2021/12/10 19:42:15 by wperu            ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "includes/stack.hpp"
+
+
+#include <iostream>       // std::cout         
+        // std::vector
+#include <deque>          // std::deque
+
+
+int main ()
+{
+  {
+  std::deque<int> mydeque (3,100);          // deque with 3 elements
+  ft::vector<int> myvector (2,200);        // vector with 2 elements
+
+  ft::stack<int> first;                    // empty stack
+  ft::stack<int,std::deque<int> > second (mydeque);         // stack initialized to copy of deque
+
+  ft::stack<int> third;  // empty stack using vector
+  ft::stack<int> fourth (myvector);
+
+  std::cout << "size of first: " << first.size() << '\n';
+  std::cout << "size of second: " << second.size() << '\n';
+  std::cout << "size of third: " << third.size() << '\n';
+  std::cout << "size of fourth: " << fourth.size() << '\n';
+  }
+
+  {
+  ft::stack<int> mystack;
+  int sum (0);
+
+  for (int i=1;i<=10;i++) mystack.push(i);
+
+  while (!mystack.empty())
+  {
+     sum += mystack.top();
+     mystack.pop();
+  }
+
+  std::cout << "total: " << sum << '\n';
+
+  
+  }
+
+  {
+  ft::stack<int> myints;
+  std::cout << "0. size: " << myints.size() << '\n';
+
+  for (int i=0; i<5; i++) myints.push(i);
+  std::cout << "1. size: " << myints.size() << '\n';
+
+  myints.pop();
+  std::cout << "2. size: " << myints.size() << '\n';
+
+
+}
+
+{
+  ft::stack<int> mystack;
+
+  mystack.push(10);
+  mystack.push(20);
+
+  mystack.top() -= 5;
+
+  std::cout << "mystack.top() is now " << mystack.top() << '\n';
+
+
+}
+
+{
+  ft::stack<int> mystack;
+
+  for (int i=0; i<5; ++i) mystack.push(i);
+
+  std::cout << "Popping out elements...";
+  while (!mystack.empty())
+  {
+     std::cout << ' ' << mystack.top();
+     mystack.pop();
+  }
+  std::cout << '\n';
+
+
+}
+{
+ft::vector<int> f(3,100);   // three ints with a value of 100
+ft::vector<int> b (2,200);   // two ints with a value of 200
+ft::stack<int> foo(f);
+ft::stack<int> bar(f);
+  if (foo==bar) std::cout << "foo and bar are equal\n";
+  if (foo!=bar) std::cout << "foo and bar are not equal\n";
+  if (foo< bar) std::cout << "foo is less than bar\n";
+  if (foo> bar) std::cout << "foo is greater than bar\n";
+  if (foo<=bar) std::cout << "foo is less than or equal to bar\n";
+  if (foo>=bar) std::cout << "foo is greater than or equal to bar\n";
+}
+  return 0;
+}
